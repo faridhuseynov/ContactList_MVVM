a07f5932f5576f0657d4d17128f07b920b3afc5a
Farid Huseynov <F.H189@hotmail.com>
2018-12-25 23:46:59 +0400

    Up to Json File created and set

---
+++ ContactList_MVVM/ViewModelLocator.cs
@@ -0,0 +1,51 @@
+﻿using Autofac;
+using Autofac.Configuration;
+using ContactList_MVVM.Services;
+using ContactList_MVVM.ViewModels;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactList_MVVM
+{
+    class ViewModelLocator
+    {
+        private AppViewModel appViewModel;
+        private ContactListViewModel contactListViewModel;
+        private AddEditContactViewModel addEditContactViewModel;
+
+        private INavigationService navigationService;
+        public static IContainer Container;
+
+        public ViewModelLocator()
+        {
+            try
+            {
+                var config = new ConfigurationBuilder();
+                config.AddJsonFile("autofac.json");
+                var module = new ConfigurationModule(config.Build());
+                var builder = new ContainerBuilder();
+                builder.RegisterModule(module);
+                Container = builder.Build();
+
+
+                navigationService = Container.Resolve<INavigationService>();
+                contactListViewModel = Container.Resolve<ContactListViewModel>();
+                addEditContactViewModel = Container.Resolve<AddEditContactViewModel>();
+
+                navigationService.Register<ContactListViewModel>(contactListViewModel);
+                navigationService.Register<AddEditContactViewModel>(addEditContactViewModel);
+
+                navigationService.Navigate<ContactListViewModel>();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
--- ContactList_MVVM/Views/AddEditContactView.xaml.cs
+++ ContactList_MVVM/Views/AddEditContactView.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ContactList_MVVM
+namespace ContactList_MVVM.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AddEditContact.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AddEditContact : UserControl
     {
-        public MainWindow()
+        public AddEditContact()
         {
             InitializeComponent();
         }
